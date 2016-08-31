73960102c4f0cc2ff66da70ef4a053071a83075d
Roger Moore <rmoore74@live.co.uk>
2016-08-31 15:24:35 +0100

    Added basic add and get rest calls

--- app/api/posts.api.js
+++ app/api/posts.api.js
@@ -2,8 +2,82 @@ var express  = require('express');
 var router   = express.Router();
 var Post     = require('mongoose').model('Post');
 
+// api route to retrieve posts
+router.get('/get', function(req, res) {
+
+    try {
+        Post.find(function(err, posts) {
+            if (err) {
+                res.json({
+                    'message': 'error retrieving posts',
+                    'success': false
+                });
+
+                console.error(err);
+            } else {
+                console.log('successfully retrieved posts');
+
+                res.json({
+                    'message': posts,
+                    'success': true
+                });
+            }
+        });
+    } catch (e) {
+        console.error(e);
+
+        res.json({
+            'message': 'error retrieving posts',
+            'success': false
+        });
+    }
+});
+
+// api route to add new post
 router.post('/add', function(req, res) {
-    res.json({ 'message': 'add post temp message' });
+
+    // create new post, following Post mongoose schema
+    var newPost = Post({
+        title   : 'test post',
+        content : 'test post content',
+        author  : 'roger moore',
+        data    : Date.now()
+    });
+
+    // try to save the post to mongodb
+    try {
+        newPost.save(function(err, data) {
+
+            // if there is an error, report and return
+            // result accordingly to user
+            if (err) {
+                console.log(err);
+
+                res.json({ 
+                    'message': 'error adding post',
+                    'success': false
+                });
+            } else {
+                // else, log that it was added successfully
+                console.log('Successfully added post: ' + data);
+
+                // report back to the user
+                res.json({ 
+                    'message': 'added post',
+                    'success': true
+                });
+            }
+        });
+    } catch (e) {
+        // if an error occurs when trying to save, log
+        console.log('Error: ' + e);
+
+        // return appropriate message to the user
+        res.json({ 
+            'message': 'error adding post',
+            'success': false
+        });
+    }
 });
 
 router.delete('/remove', function(req, res) {
